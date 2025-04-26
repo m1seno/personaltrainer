d472341e660d2d0c85d55787e1e2a8c19b040e92
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-04-26 12:58:43 +0300

    Tee oma komponentti api-pyynnöille

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,23 +1,26 @@
 import axios from "axios";
 
-const baseURL = 'https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api';
+const BASE_URL =
+  "https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api";
 
-export interface Customer{
-    firstname: string;
-    lastname: string;
-    streetaddress: string;
-    postcode: string;
-    city: string;
-    email: string;
-    phone: string;
-    _links: {
-        self: { href: string };
-        customer: { href: string };
-        trainings: { href: string };
-    };
-}
+export type Customer = {
+  firstname: string;
+  lastname: string;
+  streetaddress: string;
+  postcode: string;
+  city: string;
+  email: string;
+  phone: string;
+  _links: {
+    self: { href: string };
+    customer: { href: string };
+    trainings: { href: string };
+  };
+};
 
 export const getCustomers = async (): Promise<Customer[]> => {
-    const response = await axios.get<{ content: Customer[] }>(baseURL + '/customers');
-    return response.data.content;
+  const response = await axios.get<{ _embedded: { customers: Customer[] } }>(
+    BASE_URL + "/customers"
+  );
+  return response.data._embedded.customers;
 };
