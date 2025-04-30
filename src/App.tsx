16483007e8f5db868489226b4535d9ce4c08a1f6
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-04-30 12:20:28 +0300

    Lisää App-komponenttiin AppBar ja tee siihen navigointipainikkeet. Poista turhaksi jäänyt NavigationTabs -komponentti, siistimmän ulkoasun saavuttamiseksi

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import './App.css'
-//import Customers from './pages/Customers'
-import Trainings from './pages/Trainings'
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Toolbar,
+} from "@mui/material";
+//import "./App.css";
+import { Outlet, useNavigate } from "react-router-dom";
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <Trainings />
-    </div>
-  )
+    <Box sx={{flexGrow : 1}}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar sx={{ display: "flex", gap: 2 }}>
+        <Button
+            color="inherit"
+            onClick={() => navigate('/')}
+            sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}
+          >
+            Personal Trainer
+          </Button>
+          <Button color="inherit" onClick={() => navigate('/trainings')}>
+            Treenit
+          </Button>
+          <Button color="inherit" onClick={() => navigate('/customers')}>
+            Asiakkaat
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ mt: 3}}>
+      <Outlet />
+    </Container>
+    </Box>
+  );
 }
 
-export default App
+export default App;
