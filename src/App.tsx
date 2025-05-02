f746e210c2f9e05f0e27e15da4425ce70cdb3f76
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-05-02 17:32:56 +0300

    Lisää ToastContainer App-komponenttiin, jotta ilmoitukset näkyvvät käyttäjälle

--- src/App.tsx
+++ src/App.tsx
@@ -7,35 +7,47 @@ import {
   Toolbar,
 } from "@mui/material";
 import { Outlet, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   const navigate = useNavigate(); // Navigointifunktio sivujen välillä siirtymiseen
 
   return (
-    <Box sx={{flexGrow : 1}}>
+    <Box sx={{ flexGrow: 1 }}>
       <CssBaseline /> {/* Nollaa selaimen oletustyylit MUI:n mukaisiksi */}
       <AppBar position="static">
         {/* Yläpalkki, jossa navigointipainikkeet */}
         <Toolbar sx={{ display: "flex", gap: 2 }}>
           {/* Sovelluksen nimi toimii painikkeena kotisivulle */}
-        <Button
+          <Button
             color="inherit"
-            onClick={() => navigate('/')}
-            sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}
+            onClick={() => navigate("/")}
+            sx={{ fontWeight: "bold", fontSize: "1.5rem" }}
           >
             Personal Trainer
           </Button>
-          <Button color="inherit" onClick={() => navigate('/customers')}>
+          <Button color="inherit" onClick={() => navigate("/customers")}>
             Customers
           </Button>
-          <Button color="inherit" onClick={() => navigate('/trainings')}>
+          <Button color="inherit" onClick={() => navigate("/trainings")}>
             Workouts
           </Button>
         </Toolbar>
       </AppBar>
       {/* Pääsisältö näytetään täällä reitityksen perusteella */}
-      <Container maxWidth = "xl">
-      <Outlet />
-    </Container>
+      <Container maxWidth="xl">
+        <Outlet />
+      </Container>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </Box>
   );
 }
