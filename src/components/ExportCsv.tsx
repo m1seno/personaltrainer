3268dd98f308d7610dae869c43f6b95c4c9700f4
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-05-03 19:44:28 +0300

    Lisää try/catch -lohko ExportCsv -komponenttiin

--- src/components/ExportCsv.tsx
+++ src/components/ExportCsv.tsx
@@ -2,34 +2,40 @@ import { Button } from "@mui/material";
 import { AgGridReact } from "ag-grid-react";
 import { RefObject } from "react";
 import { CustomerAll } from "../service/api";
+import { toast } from "react-toastify";
 
 /// Tyyppimääritys, joka sisältää asiakaslistan sarakkeet
 type Props = {
-    gridRef: RefObject<AgGridReact<CustomerAll> | null>;
+  gridRef: RefObject<AgGridReact<CustomerAll> | null>;
 };
 
 /// Komponentti, joka vie asiakaslistan CSV-tiedostona
 export default function ExportCsv({ gridRef }: Props) {
-    const handleExport = () => {
-        if (gridRef.current) {
-            gridRef.current.api.exportDataAsCsv({
-                fileName: "customers.csv",
-                columnKeys: [
-                    "firstname",
-                    "lastname",
-                    "streetaddress",
-                    "postcode",
-                    "city",
-                    "email",
-                    "phone",
-                ],
-            });
-        }
-    };
+  const handleExport = () => {
+    try {
+      if (gridRef.current) {
+        gridRef.current.api.exportDataAsCsv({
+          fileName: "customers.csv",
+          columnKeys: [
+            "firstname",
+            "lastname",
+            "streetaddress",
+            "postcode",
+            "city",
+            "email",
+            "phone",
+          ],
+        });
+      }
+    } catch (error) {
+      console.error("Error exporting CSV:", error);
+      toast.error("Error exporting CSV. Please try again.");
+    }
+  };
 
-    return (
-        <Button variant="outlined" color="success" onClick={handleExport}>
-            Export to CSV
-        </Button>
-    );
+  return (
+    <Button variant="outlined" color="success" onClick={handleExport}>
+      Export to CSV
+    </Button>
+  );
 }
