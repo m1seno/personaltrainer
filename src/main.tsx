3fc16be4f37409fe5a1159fafc7a5c3c9f9e74c5
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-05-04 10:07:29 +0300

    Vaihda ReactRouterin BrowserRouter HashRouteriin, koska edellämainittu ei ole yhteensopiva github pagesin kanssa

--- src/main.tsx
+++ src/main.tsx
@@ -3,30 +3,24 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import Customers from "./pages/Customers.tsx";
 import Trainings from "./pages/Trainings.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Error from "./pages/Error.tsx";
 import "react-big-calendar/lib/css/react-big-calendar.css"; // Tuodaan kalenterin tyylit
 import Calendar from "./pages/Calendar.tsx";
-
-// Määritellään sovelluksen reititys
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,               // Sovelluksen perusrakenne (AppBar + Outlet)
-    errorElement: <Error />,        // Näytetään virhesivu, jos reititys epäonnistuu
-    children: [
-      { index: true, element: <Home /> },             // / → Etusivu
-      { path: "customers", element: <Customers /> },  // /customers → Asiakassivu
-      { path: "trainings", element: <Trainings /> },  // /trainings → Treenisivu
-      { path: "calendar", element: <Calendar />, }, // /calendar → Kalenterisivu
-    ],
-  },
-]);
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 // Alustetaan React-sovellus juurielementtiin
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+<HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>      {/* Sovelluksen perusrakenne (AppBar + Outlet) */}
+          <Route index element={<Home />} />    {/* / → Etusivu */}
+          <Route path="customers" element={<Customers />} />  {/* /customers → Asiakassivu */}
+          <Route path="trainings" element={<Trainings />} />  {/* /trainings → Treenisivu */}
+          <Route path="calendar" element={<Calendar />} />    {/* /calendar → Kalenterisivu */}
+          <Route path="*" element={<Error />} />       {/* Näytetään virhesivu, jos reititys epäonnistuu */}
+        </Route>
+      </Routes>
+    </HashRouter>  </StrictMode>
 );
